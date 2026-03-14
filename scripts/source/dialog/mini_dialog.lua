bf855f658525f1c84b6a245076be365eaad16fb3
pegn0ly <owafe001@gmail.com>
2026-03-14 19:43:47 +0300

    Speaker replacement for mini dialog

--- scripts/source/dialog/mini_dialog.lua
+++ scripts/source/dialog/mini_dialog.lua
@@ -9,28 +9,35 @@ MiniDialog.answer_for_player = {[PLAYER_1] = 6, [PLAYER_2] = 6, [PLAYER_3] = 6, 
 doFile(GetMapDataPath().."dialogs_paths.lua")
 
 MiniDialog.Start =
-function(name, alt_set, player)
+---@param name string
+---@param alt_set string|nil
+---@param player PlayerID|number|nil
+---@param speakers_replace table<string|number, string|number>|nil
+function(name, alt_set, player, speakers_replace)
   player = player or PLAYER_1
   alt_set = alt_set or "main"
-  --if not MiniDialog.Sets[name] then
-    local dialog_file = MiniDialog.Paths[name].."/script.lua"
-    -- print("Dialog script file: ", dialog_file)
-    doFile(dialog_file)
-    while not MiniDialog.Sets[name] do
-      sleep()
-    end
-  --end
-  --
+  local dialog_file = MiniDialog.Paths[name].."/script.lua"
+  doFile(dialog_file)
+  while not MiniDialog.Sets[name] do
+    sleep()
+  end
   local steps_count = -1
   for step, _ in MiniDialog.Sets[name] do
     steps_count = steps_count + 1
   end
   --
-  MiniDialog.Step(MiniDialog.Paths[name].."/", MiniDialog.Sets[name], 0, steps_count, alt_set, player)
+  MiniDialog.Step(MiniDialog.Paths[name].."/", MiniDialog.Sets[name], 0, steps_count, alt_set, player, speakers_replace)
 end
 
 MiniDialog.Step =
-function(path, set, curr_step, max_step, alt_set, player)
+---@param path string
+---@param set string
+---@param curr_step number
+---@param max_step number
+---@param alt_set string|nil
+---@param player PlayerID|number
+---@param speakers_replace table<string|number, string|number>|nil
+function(path, set, curr_step, max_step, alt_set, player, speakers_replace)
   local answers = {"/Text/next.txt", "/Text/back.txt"}
   MiniDialog.answer_for_player[player] = 6
   if curr_step == 0 then
@@ -47,24 +54,23 @@ function(path, set, curr_step, max_step, alt_set, player)
     curr_set = set[curr_step.."_main"]
     text = path..curr_step.."_main.txt"
   end
-  -- print('<color=red>MiniDialog: <color=green>curr state is ', curr_step)
-  -- print('<color=red>MiniDialog: <color=green>curr text is ', text)
+  if speakers_replace and speakers_replace[curr_set.speaker] then
+    curr_set.speaker = speakers_replace[curr_set.speaker]
+  end
   local icon = ""
   if curr_set.speaker_type == SPEAKER_TYPE_HERO then
-    icon = Hero.Params.Icon(curr_set.speaker)
+    icon = Hero.Params.Icon(curr_set.speaker) ---@diagnostic disable-line
   else
     icon = Creature.Params.Icon(curr_set.speaker)
   end
   if string.spread(icon)[1] ~= '/' then
     icon = '/'..icon
   end
-  print('<color=red>MiniDialog: <color=green>curr icon is ', icon)
-  --print(curr_state, ': ok here???')
   TalkBoxForPlayers(GetPlayerFilter(player), icon, nil,
                  text, nil,
                  'MiniDialog.Callback', 1,
                  nil,
-                 0, 0,
+                 nil, 0,
                  answers[1],
                  answers[2],
                  nil,
